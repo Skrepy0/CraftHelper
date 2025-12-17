fa9717180a1a35d7cac120bf5ad9467c368022e8
Skrepy <703189721@qq.com>
2025-12-17 20:33:25 +0800

    🎉命令注册

---
+++ src/main/java/com/crafthelper/command/ModCommands.java
@@ -0,0 +1,10 @@
+package com.crafthelper.command;
+
+import com.crafthelper.CraftHelper;
+
+public class ModCommands {
+    public static void registerModCommands(){
+        CraftHelper.LOGGER.info("Register Mod Commands");
+
+    }
+}
